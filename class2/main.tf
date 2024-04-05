f17916e19d92ade1d5ad24c04aee9cc655577286
jarkynaialym <ec2-user@ip-172-31-25-13.eu-central-1.compute.internal>
2024-04-05 01:46:50 +0000

    mmash

--- class2/main.tf
+++ class2/main.tf
@@ -4,22 +4,22 @@ provider aws {
 
 
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "deployer-key"
+#   public_key = file("~/.ssh/id_rsa.pub")
+# }
 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "kaizen-jarkynai"
+  bucket_prefix = "hello-"
   force_destroy = true
 
 }
 
 
-resource "aws_s3_object" "object" {
-    depends_on = [aws_s3_bucket.example]
-  bucket = "kaizen-jarkynai"
-  key    = "main.tf"
-  source = "main.tf"
-}
+# resource "aws_s3_object" "object" {
+#     depends_on = [aws_s3_bucket.example]
+#   bucket = "kaizen-jarkynai"
+#   key    = "main.tf"
+#   source = "main.tf"
+# }
